8e818dea7b4a856092f7faf46da36bc96451e97d
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-08-30 21:40:07 +0530

    ne

--- week5/web3class/Wallet-adpater/src/App.jsx
+++ week5/web3class/Wallet-adpater/src/App.jsx
@@ -1,30 +1,26 @@
-import React, { FC, useMemo } from 'react';
+import React from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
-
-
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { Airdrop } from './Airdrop';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 function App() {
-  return (
+    return (
+        <ConnectionProvider endpoint="https://solana-devnet.g.alchemy.com/v2/DEhoo-faZJyJO03UWMXSsKycjKrK_w9G" >
+         {/* <ConnectionProvider endpoint="https://api.devnet.solana.com"> */}
 
-    <ConnectionProvider endpoint={endpoint}>
-    <WalletProvider wallets={[]} autoConnect>
-        <WalletModalProvider>
-    <div>
-    hi there 
-    </div>
-    </WalletModalProvider>
-    </WalletProvider>
-    /</ConnectionProvider>
-  )
+           <WalletProvider wallets={[]} autoConnect>
+                <WalletModalProvider>
+                    <WalletMultiButton />
+                    <WalletDisconnectButton />
+                    <div>
+                        Hi there
+                    </div>
+                    <Airdrop />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
 }
 
-export default App
+export default App;
