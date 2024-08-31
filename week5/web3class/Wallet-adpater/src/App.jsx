dc7666a21047996030e017d1a5392dd5130945a7
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-08-31 22:01:52 +0530

    new

--- week5/web3class/Wallet-adpater/src/App.jsx
+++ week5/web3class/Wallet-adpater/src/App.jsx
@@ -13,9 +13,7 @@ function App() {
                 <WalletModalProvider>
                     <WalletMultiButton />
                     <WalletDisconnectButton />
-                    <div>
-                        Hi there
-                    </div>
+                   
                     <Airdrop />
                 </WalletModalProvider>
             </WalletProvider>
