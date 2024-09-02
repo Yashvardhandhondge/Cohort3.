e8b9f6ea4138c0f04fb692e44d108874345379b2
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-09-02 21:38:02 +0530

    Update App.jsx

--- practice/Webbased-wallet/src/App.jsx
+++ practice/Webbased-wallet/src/App.jsx
@@ -1,20 +1,20 @@
-import { generateMnemonic } from "bip39";
+import { generateMnemonic } from "bip39";// genretesmnemonic is a function from bip39 that allows to generatemnemonic 
 
 import {Buffer} from 'buffer';
 import { useState } from "react";
 
-window.Buffer = window.Buffer || Buffer
+window.Buffer = window.Buffer || Buffer//buffer are local storage of browser
 
 function App(){
-  const [mnemonic,setMnemonic]=useState('');
+  const [mnemonic,setMnemonic]=useState('');//defined a variable named mnemonic to store mnemonic phrase
 
   return(
     <div>
       <button onClick={async function(){
-        const mn = await generateMnemonic();
-        setMnemonic(mn)
+        const mn = await generateMnemonic();//just a generatemnemonic function to genratemnemonic 
+        setMnemonic(mn)// and set mnemonic set genrated menonomic
       }}>Create seed phrase</button>
-      <input type="text" value={mnemonic} />
+      <input type="text" value={mnemonic} />//an input box to disply mnemonic
     </div>
   )
 }
